d8aa73c4b1a86c1d1e163b27b7dc133b43207def
seonghoho <seonghoho@codnut.com>
2024-08-05 17:19:17 +0900

    fix: CORS 에러 해결 - 외부 api 허용

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+
+// vite는 서버구나..
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/v1': {
+        target: 'https://openapi.naver.com/',
+        changeOrigin: true
+      }
     }
   }
 })
